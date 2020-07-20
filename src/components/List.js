9c637b0da0ce24ac32ec80a20c69908108070783
ivanovad <Dasha-Iv-Alex@yandex.ru>
2020-07-20 17:10:58 +0700

    Add handle change to input

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function List(props) {
+  const [noteValue, setNoteValue] = useState('')
+  let handleChange = (e) => {
+    setNoteValue(e.target.value)
+  }
   return (
     <div className="List">
       <h4>{props.listName}</h4>
+      <hr />
+      <input type="text" onChange={handleChange} />
     </div>
   )
 }
