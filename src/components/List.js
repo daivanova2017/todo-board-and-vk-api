f057c42c3348ef896b0953fdc3a3e28ddbcf8863
ivanovad <Dasha-Iv-Alex@yandex.ru>
2020-07-23 20:45:23 +0700

    Add ability to add notes to list

--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,63 @@
 import React, { useState } from 'react'
+import { useDispatch, useSelector, shallowEqual } from 'react-redux'
+import { addNote } from '../actions/NoteCollectionActions'
+import { addNoteToList } from '../actions/ListsCollectionActions'
+import { v4 as uuidv4 } from 'uuid'
+import Note from './Note'
 
 function List(props) {
   const [noteValue, setNoteValue] = useState('')
+  const dispatch = useDispatch()
+  const allLists = useSelector((state) => state.listCollection, shallowEqual)
+  const allNotes = useSelector((state) => state.noteCollection, shallowEqual)
+
   let handleChange = (e) => {
     setNoteValue(e.target.value)
+  }
+  let handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      if (noteValue === '') {
+        alert('Empty note')
+      } else {
+        const noteID = uuidv4().slice(0, 8)
+        dispatch(addNoteToList(noteID, props.listID))
+        dispatch(addNote(noteID, noteValue))
+      }
+    }
+  }
+  let getNotes = () => {
+    let currListNotes
+    for (const key in allLists) {
+      if (key === props.listID) {
+        currListNotes = allLists[key].notes
+        break
+      }
+    }
+    if (currListNotes !== undefined) {
+      let notes = currListNotes.map((elem) => {
+        const curNote = findNote(elem)
+        return (
+          <Note name={curNote.name} ID={elem} status={curNote.status}></Note>
+        )
+      })
+      return notes
+    } else {
+      return ''
+    }
+  }
+  let findNote = (note) => {
+    for (const key in allNotes) {
+      if (key === note) {
+        return allNotes[key]
+      }
+    }
   }
   return (
     <div className="List">
       <h4>{props.listName}</h4>
       <hr />
-      <input type="text" onChange={handleChange} />
+      <input type="text" onChange={handleChange} onKeyPress={handleKeyPress} />
+      {getNotes()}
     </div>
   )
 }
