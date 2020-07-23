f6a613d10dc11d45fabc370a4947761274706eb9
ivanovad <Dasha-Iv-Alex@yandex.ru>
2020-07-23 20:40:41 +0700

    Add action which add note to list

--- src/actions/ListsCollectionActions.js
+++ src/actions/ListsCollectionActions.js
@@ -5,3 +5,11 @@ export function addList(id, list) {
     id: id,
   }
 }
+
+export function addNoteToList(noteID, listID) {
+  return {
+    type: 'ADD_NOTE_TO_LIST',
+    payload: noteID,
+    id: listID,
+  }
+}
