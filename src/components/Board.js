806a5cd7a6eb90ff8eac40e33a3822678ac52585
ivanovad <Dasha-Iv-Alex@yandex.ru>
2020-07-28 16:02:21 +0700

    Add board deletion

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { setCurrentBoard } from '../actions/CurrentBoardActions'
+import { deleteBoard } from '../actions/ListOfBoardsActions'
 import { Link } from 'react-router-dom'
 
 function Board(props) {
   const dispatch = useDispatch()
   const { name, boardID } = props
+
+  let checkDeletion = () => {
+    let isDelete = window.confirm('Вы уверены, что хотите удалить доску?')
+
+    if (isDelete) {
+      dispatch(deleteBoard(boardID))
+    }
+  }
+
   return (
-    <Link to={`/board/${boardID}`}>
-      <div className="Board" onClick={() => dispatch(setCurrentBoard(boardID))}>
-        <h2 className="Board__name">{name}</h2>
+    <div className="Board">
+      <div className="Board__delete" onClick={() => checkDeletion()}>
+        &#10008;
       </div>
-    </Link>
+      <Link to={`/board/${boardID}`}>
+        <h2
+          className="Board__name"
+          onClick={() => dispatch(setCurrentBoard(boardID))}
+        >
+          {name}
+        </h2>
+      </Link>
+    </div>
   )
 }
 
