be6b5eb4c9e131df6e4058b5ce0b522706a8bc64
ivanovad <Dasha-Iv-Alex@yandex.ru>
2020-07-28 18:26:27 +0700

    Remove unnecessary conditions

--- src/reducers/listCollectionReducer.js
+++ src/reducers/listCollectionReducer.js
@@ -4,18 +4,8 @@ const initialState = {
 
 export function listCollectionReducer(state = initialState, action) {
   switch (action.type) {
-    case 'ADD_LIST':
-      const newList = { id: action.id, name: action.payload, notes: [] }
-      return { ...state, allLists: [...state.allLists, newList] }
-    case 'ADD_NOTE_TO_LIST':
-      const listID = action.id
-      const listState = state.allLists.find((elem) => elem.id === listID)
-
-      listState.notes.push(action.payload)
-      return {
-        ...state,
-        allLists: [...state.allLists, listState],
-      }
+    case 'SET_LISTS':
+      return { ...state, allLists: action.payload }
     default:
       return state
   }
