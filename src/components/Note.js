b144b8292b0855e560ea99806fc80abe4dd5f0eb
ivanovad <Dasha-Iv-Alex@yandex.ru>
2020-07-28 19:34:52 +0700

    Add ability to delete note

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,16 +1,29 @@
 import React from 'react'
-import { changeNoteStatus } from '../actions/NoteCollectionActions'
+import { changeNoteStatus, deleteNote } from '../actions/NoteCollectionActions'
+import { removeNoteFromList } from '../actions/ListsCollectionActions'
 import { useDispatch } from 'react-redux'
 
 function Note(props) {
   const dispatch = useDispatch()
-  const { name, status, ID } = props
+  const { name, status, ID, listID } = props
+
+  let checkDeletion = () => {
+    let isDelete = window.confirm('Are you sure you want to delete this note?')
+
+    if (isDelete) {
+      dispatch(deleteNote(ID))
+      dispatch(removeNoteFromList(ID, listID))
+    }
+  }
 
   return (
     <div className={status === 'inProgress' ? 'Note' : 'Note compleated'}>
       <h3>{name}</h3>
       <div class="Note__close" onClick={() => dispatch(changeNoteStatus(ID))}>
         &#10004;
+      </div>
+      <div className="Note__delete" onClick={() => checkDeletion()}>
+        &#10008;
       </div>
     </div>
   )
