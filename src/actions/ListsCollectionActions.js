de93271227665943960f937ba5f8ced661c384ce
ivanovad <Dasha-Iv-Alex@yandex.ru>
2020-07-28 18:22:38 +0700

    Add action to delete list

--- src/actions/ListsCollectionActions.js
+++ src/actions/ListsCollectionActions.js
@@ -1,15 +1,47 @@
-export function addList(id, list) {
-  return {
-    type: 'ADD_LIST',
-    payload: list,
-    id: id,
+export function addList(id, listName) {
+  return (dispatch, getState) => {
+    const newList = {
+      id: id,
+      name: listName,
+      notes: [],
+    }
+    let newLists = [...getState().listCollection.allLists, newList]
+
+    dispatch(setLists(newLists))
+  }
+}
+
+export function deleteList(listID) {
+  return (dispatch, getState) => {
+    const allLists = [...getState().listCollection.allLists]
+
+    const listIndex = allLists.findIndex((list) => {
+      return list.id === listID
+    })
+    allLists.splice(listIndex, 1)
+
+    dispatch(setLists(allLists))
   }
 }
 
 export function addNoteToList(noteID, listID) {
+  return (dispatch, getState) => {
+    const allLists = [...getState().listCollection.allLists]
+    const curList = allLists.find((list) => list.id === listID)
+
+    curList.notes.push(noteID)
+    const listIndex = allLists.findIndex((list) => {
+      return list.id === listID
+    })
+    allLists.splice(listIndex, 1, curList)
+
+    dispatch(setLists(allLists))
+  }
+}
+
+export const setLists = (allLists) => {
   return {
-    type: 'ADD_NOTE_TO_LIST',
-    payload: noteID,
-    id: listID,
+    type: 'SET_LISTS',
+    payload: allLists,
   }
 }
