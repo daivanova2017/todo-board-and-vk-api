66e1a2476656e3696042cc305d702f9c1ee370e4
ivanovad <Dasha-Iv-Alex@yandex.ru>
2020-07-28 13:15:28 +0700

    Remove useless action

--- src/actions/CurrentBoardActions.js
+++ src/actions/CurrentBoardActions.js
@@ -2,16 +2,10 @@ import { store } from '../store/configureStore'
 
 export function setCurrentBoard(id) {
   const allBoards = store.getState().listOfBoards.boards
-  let curBoard = allBoards.find((board) => board.id === id)
+  const curBoard = allBoards.find((board) => board.id === id)
+
   return {
     type: 'SET_CURRENT_BOARD',
     payload: curBoard,
-  }
-}
-
-export function updateCurrentBoard(listID) {
-  return {
-    type: 'UPDATE_CURRENT_BOARD',
-    payload: listID,
   }
 }
