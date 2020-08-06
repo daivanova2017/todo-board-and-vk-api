8ebefb9810d61965c28ea29ed27e998965cc7495
ivanovad <Dasha-Iv-Alex@yandex.ru>
2020-08-06 19:46:26 +0700

    Add drag logic

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { changeNoteStatus, deleteNote } from '../actions/NoteCollectionActions'
 import { useDispatch } from 'react-redux'
+import { useDrag } from 'react-dnd'
+import { ItemTypes } from '../constants/ItemTypes'
 
 function Note(props) {
   const dispatch = useDispatch()
   const { name, status, ID } = props
+
+  const [, drag] = useDrag({
+    item: { type: ItemTypes.NOTE, id: ID },
+    collect: (monitor) => ({
+      item: monitor.getItem(),
+      isDragging: !!monitor.isDragging(),
+    }),
+  })
 
   let checkDeletion = () => {
     let isDelete = window.confirm('Are you sure you want to delete this note?')
@@ -15,7 +25,10 @@ function Note(props) {
   }
 
   return (
-    <div className={status === 'inProgress' ? 'Note' : 'Note compleated'}>
+    <div
+      ref={drag}
+      className={status === 'inProgress' ? 'Note' : 'Note compleated'}
+    >
       <h3>{name}</h3>
       <div class="Note__close" onClick={() => dispatch(changeNoteStatus(ID))}>
         &#10004;
