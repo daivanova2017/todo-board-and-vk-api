ac869cd473d305bb09ac91e5e4880e6bb89d6dc1
ivanovad <Dasha-Iv-Alex@yandex.ru>
2020-08-06 19:43:30 +0700

    Add dnd provider

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,21 @@ import Header from './Header'
 import BoardsPage from './BoardsPage'
 import OneBoardPage from './OneBoardPage'
 import { Switch, Route } from 'react-router-dom'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 import '../style/App.css'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={BoardsPage} />
-        <Route exact path="/board/:curID" component={OneBoardPage} />
-      </Switch>
+      <DndProvider backend={HTML5Backend}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={BoardsPage} />
+          <Route exact path="/board/:curID" component={OneBoardPage} />
+        </Switch>
+      </DndProvider>
     </div>
   )
 }
